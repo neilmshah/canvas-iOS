33ed0ed223b4c4475150284535d5d1cb1b7922ac
Neil Shah <neilmshah01@gmail.com>
2018-10-26 23:46:36 -0700

    working commit

---
+++ README.md
@@ -0,0 +1,50 @@
+
+# Lab 7 - *canvas*
+
+**canvas** is an app that allows users to use gestures in order to create fun pictures using a variety of images.
+
+Time spent: **4** hours spent in total
+
+## User Stories
+
+The following **required** user stories are complete:
+
+- [x] Add and Configure View Elements (+1pt)
+- [x] User can use a pan gesture to move the position of the tray (+1pt)
+- [x] The tray will continue to the up or down position based on the direction the user was last panning (+1pt)
+- [x] User can use a pan gesture to drag faces from the image tray to the canvas (+1pt)
+- [x] User can pan faces on the canvas (+1pt)
+
+The following **stretch** user stories are implemented:
+
+- [x] Tray bounces when returning to it's up or down positions (+1pt)
+- [x] User sees as new face scale up and back down to simulate a "picking up and dropping" appearance (+1pt)
+- [x] User can use a pinch gesture to scale faces up or down (+1pt)
+- [x] User sees the tray arrow adjust to point up or down in relation to the position of the tray (+1pt)
+- [x] User can use a double tap gesture to delete a face (+1pt)
+
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='https://i.imgur.com/fDJq40Q.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+
+## License
+
+Copyright [2018] [Neil Shah]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ canvas/ViewControllers/CanvasViewController.swift
@@ -0,0 +1,131 @@
+//
+//  CanvasViewController.swift
+//  canvas
+//
+//  Created by Neil Shah on 10/26/18.
+//  Copyright © 2018 Neil Shah. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController, UIGestureRecognizerDelegate {
+
+    @IBOutlet weak var trayArrowImageView: UIImageView!
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        trayDownOffset = 210
+        trayUp = trayView.center // The initial position of the tray
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset) // The position of the tray transposed down
+        //trayView.center = trayDown
+    }
+    
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: view)
+        
+        
+        if sender.state == .began {
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            if velocity.y>0 {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayDown
+                                self.trayArrowImageView.transform = CGAffineTransform(rotationAngle: (-180.0 * .pi) / 180.0)
+                }, completion: nil)
+            } else {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayUp
+                                self.trayArrowImageView.transform = CGAffineTransform(rotationAngle: (360.0 * .pi) / 180.0)
+                }, completion: nil)
+                
+            }
+        }
+    }
+    
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+        print("didPanFace")
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            let imageView = sender.view as! UIImageView
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            view.addSubview(newlyCreatedFace)
+            newlyCreatedFace.center = imageView.center
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+            UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                           animations: { () -> Void in
+                            self.newlyCreatedFace.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            }, completion: nil)
+            
+            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPan(sender:)))
+            newlyCreatedFace.isUserInteractionEnabled = true
+            panGestureRecognizer.delegate = self
+            newlyCreatedFace.addGestureRecognizer(panGestureRecognizer)
+            
+            let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(didPinch(sender:)))
+            newlyCreatedFace.addGestureRecognizer(pinchGestureRecognizer)
+            
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTap(sender:)))
+            tapGestureRecognizer.numberOfTapsRequired = 2
+            newlyCreatedFace.addGestureRecognizer(tapGestureRecognizer)
+            
+        } else if sender.state == .changed {
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                           animations: { () -> Void in
+                            self.newlyCreatedFace.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }, completion: nil)
+        }
+    }
+    
+    @objc func didPan(sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            newlyCreatedFace = sender.view as? UIImageView // To get the face we panned onto
+            UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                           animations: { () -> Void in
+                            self.newlyCreatedFace.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            }, completion: nil)
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center // so we can offset by translation later.
+        } else if sender.state == .changed {
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                           animations: { () -> Void in
+                            self.newlyCreatedFace.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }, completion: nil)
+        }
+    }
+    
+    @objc func didPinch(sender: UIPinchGestureRecognizer) {
+        // get the scale value from the pinch gesture recognizer
+        let scale = sender.scale
+        newlyCreatedFace.transform = CGAffineTransform(scaleX: scale, y: scale)
+        
+    }
+    
+    @objc func didTap(sender: UITapGestureRecognizer) {
+        newlyCreatedFace.removeFromSuperview()
+    }
+    
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+}
